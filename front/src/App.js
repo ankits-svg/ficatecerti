d64d45a0e56fb4a3ed0dc4afa1b33447c80616e6
Ankit Sharma <ankits745@gmail.com>
2024-01-03 11:03:16 +0530

    added hackerrank tags

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Mainroutes from './Components/Mainroutes';
-
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
+    <HelmetProvider>
     <div className="App">
       {/* <h1>WebPage</h1> */}
       <Mainroutes/>
       
     </div>
+    </HelmetProvider>
   );
 }
 
--- front/src/Components/Mainroutes.jsx
+++ front/src/Components/Mainroutes.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+// import { HelmetProvider } from 'react-helmet-async';
 import Frontpage from '../Pages/Frontpage';
 import Displaypage from '../Pages/Displaypage';
 
 const Mainroutes = () => {
   return (
-    <HelmetProvider>
+    
       <Routes>
         <Route path="/" element={<Frontpage />} />
         <Route path="/display/:id" element={<Displaypage />} />
       </Routes>
-    </HelmetProvider>
+   
   );
 };
 
